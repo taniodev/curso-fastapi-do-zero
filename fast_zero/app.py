e65cf56faba28321a5d021d847c6e6e7bef98c4f
Tânio Scherer <tanioms3@gmail.com>
2024-10-02 11:22:10 -0300

    Aula 2 exercício: Retorna 'olá mundo' em HTML

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,6 +1,7 @@
 from http import HTTPStatus
 
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 from fast_zero.schemas import Message
 
@@ -10,3 +11,12 @@ app = FastAPI()
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+@app.get('/hello', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def read_root_html():
+    return """
+    <html>
+      <head><title>Hello HTML</title></head>
+      <body><p>Olá Mundo!</p></body>
+    </html>"""
--- tests/test_app.py
+++ tests/test_app.py
@@ -11,3 +11,18 @@ def test_root_retorna_ok_e_ola_mundo():
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo!'}
+
+
+def test_root_retorna_ola_mundo_html():
+    client = TestClient(app)
+    response = client.get('/hello')
+
+    assert response.status_code == HTTPStatus.OK
+    assert (
+        response.text
+        == """
+    <html>
+      <head><title>Hello HTML</title></head>
+      <body><p>Olá Mundo!</p></body>
+    </html>"""
+    )
