144bad81596ff182faa4d087e152d8224c456011
Tânio Scherer <tanioms3@gmail.com>
2024-10-04 12:40:05 -0300

    Cria um teste para a rota POST

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,20 +1,14 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
-
-
-def test_root_retorna_ok_e_ola_mundo():
-    client = TestClient(app)
+def test_root_retorna_ok_e_ola_mundo(client):
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo!'}
 
 
-def test_root_retorna_ola_mundo_html():
-    client = TestClient(app)
+def test_root_retorna_ola_mundo_html(client):
     response = client.get('/hello')
 
     assert response.status_code == HTTPStatus.OK
@@ -26,3 +20,21 @@ def test_root_retorna_ola_mundo_html():
       <body><p>Olá Mundo!</p></body>
     </html>"""
     )
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'melancia',
+            'email': 'melancia@example.com',
+            'password': 'secret',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'melancia',
+        'email': 'melancia@example.com',
+        'id': 1,
+    }
