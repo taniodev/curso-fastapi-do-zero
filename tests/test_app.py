e95b59229500d5657c0839a5c9284bb3fc7ba809
Tânio Scherer <tanioms3@gmail.com>
2024-10-11 13:29:09 -0300

    Aula 05: Passa a utilizar um banco de dados real
    Também atualiza os testes para refletir essas mudanças.

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+from fast_zero.schemas import UserPublic
+
 
 def test_root_retorna_ok_e_ola_mundo(client):
     response = client.get('/')
@@ -41,21 +43,20 @@ def test_create_user(client):
 
 
 def test_read_users(client):
+    response = client.get('/users')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'users': []}
+
+
+def test_read_users_with_users(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
     response = client.get('/users/')
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'users': [
-            {
-                'username': 'melancia',
-                'email': 'melancia@example.com',
-                'id': 1,
-            }
-        ]
-    }
+    assert response.json() == {'users': [user_schema]}
 
 
-def test_update_user(client):
+def test_update_user(client, user):
     response = client.put(
         '/users/1',
         json={
@@ -89,25 +90,32 @@ def test_update_user_not_found(client):
     }
 
 
-def test_get_user(client):
-    response = client.get('/users/1')
+def test_update_integrity_error(client, user):
+    client.post(
+        '/users',
+        json={
+            'username': 'moranguinho',
+            'email': 'moranguinho@example.com',
+            'password': 'secret',
+        },
+    )
 
-    assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'username': 'moranguinho',
-        'email': 'moranguinho@example.com',
-        'id': 1,
+    response_update = client.put(
+        f'/users/{user.id}',
+        json={
+            'username': 'moranguinho',
+            'email': 'bob@example.com',
+            'password': 'password',
+        },
+    )
+
+    assert response_update.status_code == HTTPStatus.CONFLICT
+    assert response_update.json() == {
+        'detail': 'Username or e-mail already exists'
     }
 
 
-def test_get_user_not_found(client):
-    response = client.get('/users/9')
-
-    assert response.status_code == HTTPStatus.NOT_FOUND
-    assert response.json() == {'detail': 'User not found'}
-
-
-def test_delete_user(client):
+def test_delete_user(client, user):
     response = client.delete('/users/1')
 
     assert response.status_code == HTTPStatus.OK
@@ -116,6 +124,21 @@ def test_delete_user(client):
 
 def test_delete_user_not_found(client):
     response = client.delete('/users/9')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
+
+
+def test_get_user(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == user_schema
+
+
+def test_get_user_not_found(client):
+    response = client.get('/users/9')
 
     assert response.status_code == HTTPStatus.NOT_FOUND
     assert response.json() == {'detail': 'User not found'}
