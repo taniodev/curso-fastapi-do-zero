f43eedbe1100218956c0d9d63930feaee534ff2c
Tânio Scherer <tanioms3@gmail.com>
2024-10-15 07:34:44 -0300

    Aula 06: Cria uma função para gerar token JWT

---
+++ fast_zero/security.py
@@ -0,0 +1,20 @@
+from datetime import datetime, timedelta
+
+from jwt import encode
+from pwdlib import PasswordHash
+from zoneinfo import ZoneInfo
+
+SECRET_KEY = 'uma-coisa-secreta'
+ALGORITHM = 'HS256'
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+pwd_context = PasswordHash.recommended()
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.now(tz=ZoneInfo('UTC')) + timedelta(
+        minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+    )
+    to_encode.update({'exp': expire})
+    encoded_jwt = encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
