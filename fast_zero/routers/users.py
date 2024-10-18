0d067673dff3cdc5d18f8831bd9a5028bf18ff00
Tânio Scherer <tanioms3@gmail.com>
2024-10-18 12:24:08 -0300

    Aula 07: Usa tipos Annotated nas rotas da API

--- fast_zero/routers/users.py
+++ fast_zero/routers/users.py
@@ -1,4 +1,5 @@
 from http import HTTPStatus
+from typing import Annotated
 
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import select
@@ -14,10 +15,12 @@ from fast_zero.security import (
 )
 
 router = APIRouter(prefix='/users', tags=['users'])
+CurrentUser = Annotated[User, Depends(get_current_user)]
+Session = Annotated[Session, Depends(get_session)]
 
 
 @router.post('/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
-def create_user(user: UserSchema, session: Session = Depends(get_session)):
+def create_user(user: UserSchema, session: Session):
     db_user = session.scalar(
         select(User).where(
             (User.username == user.username) | (User.email == user.email)
@@ -51,7 +54,9 @@ def create_user(user: UserSchema, session: Session = Depends(get_session)):
 
 @router.get('/', response_model=UserList)
 def read_users(
-    skip: int = 0, limit: int = 100, session: Session = Depends(get_session)
+    session: Session,
+    skip: int = 0,
+    limit: int = 100,
 ):
     users = session.scalars(select(User).offset(skip).limit(limit)).all()
     return {'users': users}
@@ -61,8 +66,8 @@ def read_users(
 def update_user(
     user_id: int,
     user: UserSchema,
-    session: Session = Depends(get_session),
-    current_user: User = Depends(get_current_user),
+    session: Session,
+    current_user: CurrentUser,
 ):
     if current_user.id != user_id:
         raise HTTPException(
@@ -87,8 +92,8 @@ def update_user(
 @router.delete('/{user_id}', response_model=Message)
 def delete_user(
     user_id: int,
-    session: Session = Depends(get_session),
-    current_user: User = Depends(get_current_user),
+    session: Session,
+    current_user: CurrentUser,
 ):
     if current_user.id != user_id:
         raise HTTPException(
@@ -102,7 +107,7 @@ def delete_user(
 
 
 @router.get('/{user_id}', response_model=UserPublic)
-def get_user(user_id: int, session: Session = Depends(get_session)):
+def get_user(user_id: int, session: Session):
     db_user = session.scalar(select(User).where(User.id == user_id))
 
     if not db_user:
