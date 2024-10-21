7765c5ee24d62b181780d6e9754f784085200831
Tânio Scherer <tanioms3@gmail.com>
2024-10-21 14:23:30 -0300

    Aula 08: Testa a expiração do token JWT

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+from freezegun import freeze_time
+
 
 def test_get_access_token(client, user):
     response = client.post(
@@ -11,3 +13,27 @@ def test_get_access_token(client, user):
     assert response.status_code == HTTPStatus.OK
     assert 'access_token' in token
     assert 'token_type' in token
+
+
+def test_token_expired_after_time(client, user):
+    with freeze_time('2024-01-01 12:00:00'):
+        response = client.post(
+            '/auth/token',
+            data={'username': user.email, 'password': user.clean_password},
+        )
+        assert response.status_code == HTTPStatus.OK
+        token = response.json()['access_token']
+
+    with freeze_time('2024-01-01 12:31:00'):
+        response = client.put(
+            f'/users/{user.id}',
+            headers={'Authorization': f'Bearer {token}'},
+            json={
+                'username': 'melancia',
+                'email': 'melancia@test.com',
+                'password': 'secret',
+            },
+        )
+
+        assert response.status_code == HTTPStatus.UNAUTHORIZED
+        assert response.json() == {'detail': 'Could not validate credentials'}
