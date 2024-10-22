8698f39e44168bd4e195d42060e2cf5e8aaa6354
Tânio Scherer <tanioms3@gmail.com>
2024-10-22 10:37:11 -0300

    Aula 09: Cria os modelos do banco de dados

--- fast_zero/models.py
+++ fast_zero/models.py
@@ -1,7 +1,8 @@
 from datetime import datetime
+from enum import Enum
 
-from sqlalchemy import func
-from sqlalchemy.orm import Mapped, mapped_column, registry
+from sqlalchemy import ForeignKey, func
+from sqlalchemy.orm import Mapped, mapped_column, registry, relationship
 
 table_registry = registry()
 
@@ -20,3 +21,27 @@ class User:
     updated_at: Mapped[datetime] = mapped_column(
         init=False, server_default=func.now(), onupdate=func.now()
     )
+    todos: Mapped[list['Todo']] = relationship(
+        init=False, back_populates='user', cascade='all, delete-orphan'
+    )
+
+
+class TodoState(str, Enum):
+    draft = 'draft'
+    todo = 'todo'
+    doing = 'doing'
+    done = 'done'
+    trash = 'trash'
+
+
+@table_registry.mapped_as_dataclass
+class Todo:
+    __tablename__ = 'todos'
+
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    title: Mapped[str]
+    description: Mapped[str]
+    state: Mapped[TodoState]
+
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+    user: Mapped[User] = relationship(init=False, back_populates='todos')
--- tests/test_db.py
+++ tests/test_db.py
@@ -2,7 +2,7 @@ from dataclasses import asdict
 
 from sqlalchemy import select
 
-from fast_zero.models import User
+from fast_zero.models import Todo, User
 
 
 def test_create_user(session, mock_db_time):
@@ -22,4 +22,22 @@ def test_create_user(session, mock_db_time):
         'email': 'melancia@teste.com',
         'created_at': time,
         'updated_at': time,
+        'todos': [],
     }
+
+
+def test_create_todo(session, user: User):
+    todo = Todo(
+        title='My test todo',
+        description='Test description',
+        state='draft',
+        user_id=user.id,
+    )
+
+    session.add(todo)
+    session.commit()
+    session.refresh(todo)
+
+    user = session.scalar(select(User).where(User.id == user.id))
+
+    assert todo in user.todos
